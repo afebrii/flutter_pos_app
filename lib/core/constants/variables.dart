23a5261db044e3dc25f4c2356fdeaf1796bbd967
andika febriansyah <82563655+afebrii@users.noreply.github.com>
2024-08-08 13:18:51 +0700

    Login, Logout, Save Local Auth

---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,3 @@
+class Variables {
+  static const String baseUrl = 'http://192.168.43.187:8000';
+}
---
+++ lib/presentation/home/pages/dashboard_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_pos_app/data/datasources/auth_local_datasource.dart';
+import 'package:flutter_pos_app/presentation/auth/pages/login_page.dart';
+import 'package:flutter_pos_app/presentation/home/bloc/logout_bloc.dart';
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+        actions: [
+          BlocConsumer<LogoutBloc, LogoutState>(
+            listener: (context, state) {
+              state.maybeMap(
+                orElse: () {},
+                success: (_) {
+                  AuthLocalDatasource().removeAuthData();
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const LoginPage()));
+                },
+              );
+            },
+            builder: (context, state) {
+              return IconButton(
+                  onPressed: () {
+                    context.read<LogoutBloc>().add(const LogoutEvent.logout());
+                  },
+                  icon: const Icon(Icons.logout));
+            },
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text('Dashboard'),
+      ),
+    );
+  }
+}
